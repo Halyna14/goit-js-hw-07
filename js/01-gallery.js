c30685a0a5449171d994569f702d88634331e9c2
Halyna Melnychuk <106915592+Halyna14@users.noreply.github.com>
2022-11-17 01:20:10 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryRef.addEventListener("click", onGalleryItemsClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>`
+    )
+    .join(" ");
+}
+
+function onGalleryItemsClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains(`gallery__image`)) {
+    return;
+  }
+
+  const source = event.target.dataset.source;
+  openWindow(source);
+}
+function openWindow(source) {
+  const instance = basicLightbox.create(
+    `<img src="${source}" width="900" height="600">`
+  );
+  instance.show();
+  document.addEventListener("keydown", closeWindow);
+
+  function closeWindow(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", closeWindow);
+    }
+  }
+}
 
 console.log(basicLightbox);
